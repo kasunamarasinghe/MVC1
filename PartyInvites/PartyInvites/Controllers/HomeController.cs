2937ae85e325bbb2a9eff5b84acc87771e476b4f
kasunamarasinghe <kasun@davton.com>
2015-09-21 12:02:07 +0530

    Display Hello World in the web page

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -11,9 +11,9 @@ namespace PartyInvites.Controllers
         //
         // GET: /Home/
 
-        public ActionResult Index()
+        public String Index()
         {
-            return View();
+            return "HelloWorld";
         }
 
     }
